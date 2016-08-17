af8bc5a35314d5989502f6dbdcc85115e5242f7c
Derek Petersen <derek@salesrabbit.com>
2016-08-16 18:23:54 -0600

    Migration script

---
+++ scripts/mysqlMigrations.php
@@ -0,0 +1,35 @@
+<?
+
+require_once("../init.php");
+
+$con = DatabaseConnect::getConnection();
+
+$sql = "CREATE TABLE `AutoMatch` (
+  `AutoMatchID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `DateCreated` datetime NOT NULL,
+  PRIMARY KEY (`AutoMatchID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;";
+$con->exec($sql);
+
+$sql = "CREATE TABLE `AutoMatchTemplate` (
+  `AutoMatchTemplateID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `AutoMatchID` int(11) unsigned NOT NULL,
+  `CsvField` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `TableField` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`AutoMatchTemplateID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;";
+$con->exec($sql);
+
+$sql = "CREATE TABLE `User` (
+  `UserID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `LastName` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `Address` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `City` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `State` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `Zip` int(10) NOT NULL,
+  `Score` float NOT NULL,
+  `DateCreated` datetime NOT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;";
+$con->exec($sql);
